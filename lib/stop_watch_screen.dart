69e4561b49d4dbb2862ca7b40437f6ff3d205e91
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-14 23:07:26 +0900

    066. 스톱 워치 프로젝트 생성

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
