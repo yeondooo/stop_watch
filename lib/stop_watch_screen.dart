7d1d6135d1dee6b1d5015ed580c4fae6da3a150a
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-15 00:14:11 +0900

    073. 시작,일시정지 상태 변환 로직

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -17,6 +17,20 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
 
   List<String> _lapTimes = [];
 
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {}
+
+  void _pause() {}
+
   @override
   void dispose() {
     _timer?.cancel();
@@ -65,8 +79,14 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
                 child: const Icon(Icons.refresh),
               ),
               FloatingActionButton(
-                onPressed: () {},
-                child: const Icon(Icons.play_arrow),
+                onPressed: () {
+                  setState(() {
+                    _clickButton();
+                  });
+                },
+                child: _isRunning
+                    ? const Icon(Icons.pause)
+                    : const Icon(Icons.play_arrow),
               ),
               FloatingActionButton(
                 onPressed: () {},
